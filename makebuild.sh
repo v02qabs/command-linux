b5d74a82a74509df836546664bc81d312279e477
v02qabs <takesue090@gmail.com>
2020-01-14 00:17:15 +0900

    tools.sh

---
+++ makebuild.sh
@@ -0,0 +1,4 @@
+    ~/document/devel/Android/sdk/ndk/20.1.5948944/build/tools/make_standalone_toolchain.py \
+      --arch arm64 \
+      --api 28 \
+      --install-dir=my-toolchain
--- psp-conversion-by-ffmpeg/psp-video.sh
+++ psp-conversion-by-ffmpeg/psp-video.sh
@@ -1,1 +1,1 @@
-ffmpeg -y -i $1 -flags +bitexact -vcodec libx264 -profile:v baseline -level 3.0 -s 480x272 -r 29.97 -b:v 384k -acodec aac -b:a 96k -ar 48000 -f psp -strict -2 $1.MP4
+ffmpeg -y -i $1 -flags +bitexact -vcodec libx264 -profile:v baseline -level 3.0 -s 480x272 -r 29.97 -b:v 384k -acodec aac -b:a 96k -ar 48000 -f psp -strict -2 $HOME/video/psp-video/$1.MP4
